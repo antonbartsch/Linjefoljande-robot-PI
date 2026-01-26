e36b5d4d8e27c632999cba7498e7275e284840a2
antonbartsch <anton.bartsch@outlook.com>
2026-01-26 10:30:34 +0100

    La till funktion readSensors för att läsa sensorerna

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 
+const int leftSensorIn;
+const int rightSensorIn;
+const int leftMotorOut;
+const int rightMotorOut;
+
+
 // put function declarations here:
 int myFunction(int, int);
+
+int readSensors(int, int)
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -15,4 +24,25 @@ void loop() {
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+/** Läser två sensorer.
+  * Returnerar: 1=höger, -1=vänster, 0=rakt fram 
+  */
+int readSensors(int leftSensor, int rightSensor){
+  int leftSensorValue = digitalRead(leftSensor);
+  int rightSensorValue = digitalRead(rightSensor);
+  if((leftSensorValue == HIGH) && (rightSensorValue == HIGH)){
+    Serial.println("Fel! båda sensorerna ger positiv");
+    return 0;
+  }
+  else if(rightSensorValue == 1){
+    return 1;
+  }
+  else if(leftSensorValue == 1){
+    return -1;
+  }
+  else{
+    return 0;
+  }
 }
